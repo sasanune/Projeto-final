199268566b3dce2225cada19317e29e50120eb9e
banana. <giovanna.rocha.cardoso@gmail.com>
2024-10-03 14:31:11 -0300

    pagina de login

---
+++ projeto final/scripts/login.js
@@ -0,0 +1,21 @@
+const sign_in_btn = document.querySelector("#sign-in-btn");
+const sign_up_btn = document.querySelector("#sign-up-btn");
+const container = document.querySelector(".container");
+
+sign_up_btn.addEventListener('click', () =>{
+    container.classList.add("sign-up-mode");
+});
+
+sign_in_btn.addEventListener('click', () =>{
+    container.classList.remove("sign-up-mode");
+});
+
+const toggleButton = document.getElementById("toggle-dark-mode");
+toggleButton.addEventListener("click", () => {
+  document.body.classList.toggle("dark-mode");
+  if (document.body.classList.contains("dark-mode")) {
+    toggleButton.textContent = "Modo Claro";
+  } else {
+    toggleButton.textContent = "Modo Noturno";
+  }
+});
