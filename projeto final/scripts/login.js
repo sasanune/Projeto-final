30c327321e701bb0fe1732b595e00b1956ab0912
banana. <giovanna.rocha.cardoso@gmail.com>
2024-10-08 21:59:06 -0300

    add firebase to login page

--- projeto final/scripts/login.js
+++ projeto final/scripts/login.js
@@ -1,3 +1,42 @@
+
+document.getElementById('login-btn').addEventListener('click', () => {
+  const email = document.getElementById('login-email').value;
+  const password = document.getElementById('login-password').value;
+
+  signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      alert('Login realizado com sucesso!');
+      window.location.href = "main.html";
+    })
+    .catch((error) => {
+      alert('Erro no login: ' + error.message);
+    });
+});
+
+
+document.getElementById('signup-btn').addEventListener('click', () => {
+  const email = document.getElementById('signup-email').value;
+  const password = document.getElementById('signup-password').value;
+
+  createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      alert('Cadastro realizado com sucesso!');
+      window.location.href = "main.html";
+    })
+    .catch((error) => {
+      alert('Erro no cadastro: ' + error.message);
+    });
+});
+
+
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    toggleAuthState(true);
+  } else {
+    toggleAuthState(false);
+  }
+});
+
 const sign_in_btn = document.querySelector("#sign-in-btn");
 const sign_up_btn = document.querySelector("#sign-up-btn");
 const container = document.querySelector(".container");
